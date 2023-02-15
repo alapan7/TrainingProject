f0fdadb4f2da5376a79ad1a4a2ef67a03bd4d0d1
alapan7 <alapanghosh3@gmail.com>
2023-02-15 10:59:47 +0530

    initial commit

---
+++ TelephonicDirectory/src/test/java/com/example/TelephonicDirectory/TelephonicDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TelephonicDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephonicDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
